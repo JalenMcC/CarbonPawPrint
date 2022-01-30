51733ba9d11c6140704bfcc433f46a3fcb5cd3b6
Spacenin <chepstudios@gmail.com>
2022-01-29 21:05:02 -0500

    Added Google Maps test map

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 //import Config from "config"
 
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+
+    center: {lat: -34.987, lng: 150.644},
+    zoom: 8,
+    });
+}
+
 //var mykey ="Bearer " + Config.config.MY_KEY;
 let total_electricity_value = 0;
 
